f70607927b5d92bdb07e8357aa57d371de85eec8
zeusx <zeusxcs@gmail.com>
2025-06-10 20:26:54 +0200

    x
    x

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'users',
     'wallets',
     'transactions',
-    'goals',
+    'goals', 
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'config.urls'
 
